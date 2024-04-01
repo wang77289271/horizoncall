b376c3fa518e326f62c7895ac990c3ac4e5d6dd2
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-03-31 19:59:36 -0600

    add clerk logins & home page layout

---
+++ constants/index.ts
@@ -0,0 +1,72 @@
+export const sidebarLinks = [
+  {
+    imgURL: '/icons/Home.svg',
+    route: '/',
+    label: 'Home',
+  },
+
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/upcoming',
+    label: 'Upcoming',
+  },
+  {
+    imgURL: '/icons/previous.svg',
+    route: '/previous',
+    label: 'Previous',
+  },
+  {
+    imgURL: '/icons/Video.svg',
+    route: '/recordings',
+    label: 'Recordings',
+  },
+  {
+    imgURL: '/icons/add-personal.svg',
+    route: '/personal-room',
+    label: 'Personal Room',
+  },
+]
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+]
+
+// export const homeCardProps = [
+//   {
+//     id: 1,
+//     img: '/icons/add-meeting.svg',
+//     title: 'New Meeting',
+//     desc: 'Start an instant meeting',
+//     bgColor: 'bg-orange-1',
+//     clickState: 'isInstantMeeting',
+//   },
+//   {
+//     id: 2,
+//     img: '/icons/join-meeting.svg',
+//     title: 'Join Meeting',
+//     desc: 'via invitation link',
+//     bgColor: 'bg-blue-1',
+//     clickState: 'isJoiningMeeting',
+//   },
+//   {
+//     id: 3,
+//     img: '/icons/schedule.svg',
+//     title: 'Schedule Meeting',
+//     desc: 'Plan your meeting',
+//     bgColor: 'bg-purple-1',
+//     clickState: 'isScheduleMeeting',
+//   },
+//   {
+//     id: 4,
+//     img: '/icons/recordings.svg',
+//     title: 'View Recordings',
+//     desc: 'Meeting Recordings',
+//     bgColor: 'bg-yellow-1',
+//     clickState: '/recordings',
+//     isRecordings: true,
+//   },
+// ]
