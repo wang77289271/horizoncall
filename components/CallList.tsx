25cedbe9631822f53e073b3fbe96838e1a97b197
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-04-02 11:22:34 -0600

    end call redirecting to '/'

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -76,9 +76,9 @@ const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
                 : '/icons/recordings.svg'
             }
             title={
-              (meeting as Call).state?.custom?.description ||
+              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
               (meeting as CallRecording).filename?.substring(0, 20) ||
-              'No Description'
+              'Personal Meeting'
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
